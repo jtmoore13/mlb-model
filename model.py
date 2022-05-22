1a5ef6294eda4523ffce258f243f5f8293c9217a
Justin Moore <jtmoore@stanford.edu>
2022-05-22 16:48:13 -0700

    Test and improve model

--- model.py
+++ model.py
@@ -2,9 +2,7 @@
 import numpy as np
 from numpy import mean
 import pandas as pd
-from sklearn.compose import TransformedTargetRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, RandomizedSearchCV, KFold
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
 from sklearn.metrics import mean_squared_error
 from matplotlib import pyplot as plt
 import xgboost as xgb
@@ -14,13 +12,13 @@ import pickle
 import json
 import datetime
 import sys
+import random
 import data_utils, model_utils
 
 
 START_YEAR = 2010
 END_YEAR = 2022
 GAME_DATA, PITCHER_DATA, BULLPEN_DATA = data_utils.get_data_dicts(START_YEAR, END_YEAR)
-RUN_MAX = 100
 MODEL_FILE = 'my_model.sav'
 
 FEATURE_LIST = [
@@ -38,7 +36,8 @@ FEATURE_LIST = [
     'opp_bullpen_WHIP',
     'stadium_score',
     'temp',
-    'over_under'
+    'over_under',
+    'has_DH',
 ]
 
 def make_game_sample(game, year, team, date):
@@ -47,6 +46,7 @@ def make_game_sample(game, year, team, date):
     sample = []             
     throw = 'right' if game['opp_starter_righty'] else 'left'
     loc = 'home' if game['home'] else 'away'
+    home_team = team if game['home'] else game['opp']
     sample.append(game['pregame_BA'])
     sample.append(game['pregame_SLG'])
     sample.append(game['10-day_BA'])
@@ -62,6 +62,7 @@ def make_game_sample(game, year, team, date):
     sample.append(data_utils.get_stadium_score(team, game))
     sample.append(game['temp'])
     sample.append(game['over_under'])
+    sample.append(data_utils.has_DH(home_team, year))
     return np.array(sample)
 
 
@@ -70,8 +71,6 @@ def is_incomplete_sample(date, game):
     Return true if the game contains all of the necessary info
     for a full sample. 
     """
-    if game['R'] > RUN_MAX:
-        return True
     if 'opp_starter_id' not in game or game['opp_starter_id'] == 'not_found':
         return True
     if 'pregame_BA' not in game:
@@ -115,24 +114,23 @@ def get_samples(test_years):
     train_targets = []
     test_samples = []
     test_targets = []
-
-
     for year in GAME_DATA:
         for team in GAME_DATA[year]:
             for date in list(GAME_DATA[year][team])[1:]:
                 game = GAME_DATA[year][team][date]
+                team_runs = min(game['R'], 9)
                 if is_incomplete_sample(date, game):
                     incomplete += 1
                     continue
                 sample = make_game_sample(game, year, team, date)
                 samples.append(sample)
-                targets.append(game['R'])
+                targets.append(team_runs)
                 if year not in test_years:
                     train_samples.append(sample)
-                    train_targets.append(game['R'])
+                    train_targets.append(team_runs)
                 else:
                     test_samples.append(sample)
-                    test_targets.append(game['R'])
+                    test_targets.append(team_runs)
 
     print(f'{Style.DIM}{len(samples)} complete samples.')
     print(f'{incomplete} incomplete samples.\n{Style.RESET_ALL}')
@@ -146,29 +144,64 @@ def get_samples(test_years):
     return df, train_df, test_df
 
 
-def compare_to_vegas(rf, years):
+def compare_to_vegas(rf, test_years, nearest_half=True):
     """
     """
-    for year in years:
+    me_closer = 0
+    vegas_closer = 0
+    total = 1
+    over_vegas = 0
+    under_vegas = 0
+    push_vegas = 0
+    games = set()
+    for year in test_years:
         for team in GAME_DATA[year]:
             for date in GAME_DATA[year][team]:
                 game1 = GAME_DATA[year][team][date]
                 game2 = GAME_DATA[year][game1['opp']][date]
+                home_team = team if game1['home'] else game1['opp']
+                away_team = game1['opp'] if home_team == team else team
+                game_id = home_team + away_team + date
+                if game_id in games:
+                    continue
                 if is_incomplete_sample(date, game1) or is_incomplete_sample(date, game2):
                     continue
-                sample1 = make_game_sample(game1, year, team, date).reshape(1, 15)
-                sample2 = make_game_sample(game2, year, game1['opp'], date).reshape(1, 15)
+
+                sample1 = make_game_sample(game1, year, team, date).reshape(1, len(FEATURE_LIST))
+                sample2 = make_game_sample(game2, year, game1['opp'], date).reshape(1, len(FEATURE_LIST))
                 prediction1 = float(rf.predict(sample1)[0])
                 prediction2 = float(rf.predict(sample2)[0])
 
                 my_total = round(prediction1 + prediction2, 2)
+                if nearest_half:
+                    my_total = round(my_total*2)/2
                 vegas_total = game1['over_under']
                 actual_total = game1['R'] + game2['R']
 
-                print(f'{team} vs {game1["opp"]} {date}')
-                print(f'My total:     {my_total}')
-                print(f'Vegas total:  {vegas_total}')
-                print(f'Actual score: {actual_total}\n')
+                if abs(my_total - vegas_total) >= 0:
+                    total += 1
+                    games.add(game_id)
+                    if my_total > vegas_total:
+                        over_vegas += 1
+                    elif my_total < vegas_total:
+                        under_vegas += 1
+                    else:
+                        push_vegas += 1
+
+                    if abs(my_total - actual_total) < abs(vegas_total - actual_total):
+                        me_closer += 1
+                    elif abs(my_total - actual_total) > abs(vegas_total - actual_total):
+                        vegas_closer += 1
+
+                # print(f'{team} vs {game1["opp"]} {date}')
+                # print(f'My total:     {my_total}')
+                # print(f'Vegas total:  {vegas_total}')
+                # print(f'Actual score: {actual_total}\n')
+
+    print(f'over vegas: {round(over_vegas*100/total)}%, under vegas: {round(under_vegas*100/total)}%, same: {round(push_vegas*100/total)}%')
+    print('---------------------------')
+    print(f'Model success {test_years[0]}: {round(me_closer*100/(total-push_vegas), 2)}%')
+    print('---------------------------')
 
 
 def develop(rf, df):
@@ -204,12 +237,6 @@ def develop(rf, df):
     plt.savefig('figures/feature_importance.png')
     plt.close()
 
-    # compare to previous best model
-    best_log = model_utils.best_model_log()
-    if test_rmse < best_log['best_rmse']:
-        model_utils.set_best_model(test_rmse, RUN_MAX, FEATURE_LIST)
-        print(Fore.GREEN+'New best model!'+Style.RESET_ALL)
-
 
 def fit_and_save(rf, df, filename):
     """
@@ -219,11 +246,23 @@ def fit_and_save(rf, df, filename):
     pickle.dump(rf, open(filename, 'wb'))
  
 
+def test_each_year():
+    """
+    """
+    for year in [str(year) for year in range(2021, 2022) if year != 2020]:
+        rf = xgb.XGBRFRegressor(n_estimators=1000, max_depth=7, gamma=0, min_child_weight=7, subsample=.6, reg_alpha=.1)
+        test_years = [year]
+        df, train_df, test_df = get_samples(test_years)
+        X_train, y_train = train_df.loc[:, train_df.columns!='runs_scored'], train_df['runs_scored']
+        rf.fit(X_train, y_train)
+        compare_to_vegas(rf, test_years, nearest_half=True)
+
+
 def main():
     """
     """
     rf = xgb.XGBRFRegressor(n_estimators=1000, max_depth=7, gamma=0, min_child_weight=7, subsample=.6, reg_alpha=.1)
-    test_years = ['2018', '2019', '2021']
+    test_years = ['2021']
     df, train_df, test_df = get_samples(test_years)
 
     args = sys.argv
@@ -234,7 +273,7 @@ def main():
     elif '-c' in args or '-compare' in args:
         X_train, y_train = train_df.loc[:, train_df.columns!='runs_scored'], train_df['runs_scored']
         rf.fit(X_train, y_train)
-        compare_to_vegas(rf, test_years)
+        compare_to_vegas(rf, test_years, nearest_half=True)
 
 
 if __name__ == '__main__':
